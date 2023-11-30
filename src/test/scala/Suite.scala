21be7bcee73abe2b81ca5ac38bfbafc8f0617da8
Fabian Achammer <fabian@achammer.dev>
2023-11-30 22:48:43 +0100

    rename test suite

--- src/test/scala/Suite.scala
+++ src/test/scala/Suite.scala
@@ -1,6 +1,6 @@
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
-class MySuite extends munit.FunSuite {
+class Suite extends munit.FunSuite {
   test("example test that succeeds") {
     val obtained = 42
     val expected = 42
