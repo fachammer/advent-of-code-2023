78ba9e64982a7d95767f18cffac3ca4d21805446
Fabian Achammer <fabian@achammer.dev>
2023-11-30 22:36:51 +0100

    initial setup

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "A very basic flake";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/23.11"; };
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+    in {
+      devShells.aarch64-darwin.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ scala_3 sbt nil nixfmt jdk17 ];
+      };
+    };
+}
