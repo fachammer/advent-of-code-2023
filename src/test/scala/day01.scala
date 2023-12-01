92112495cad3e564a0782fe30ded0ddddd651364
Fabian Achammer <fabian@achammer.dev>
2023-12-01 22:43:33 +0100

    align prettier

--- src/test/scala/day01.scala
+++ src/test/scala/day01.scala
@@ -20,27 +20,27 @@ class part2 extends test.DayPart(1) {
     input.linesIterator.map(calibrationValue).sum
 
   calibrationValue.testCases(
-    "two1nine" -> 29,
-    "eightwothree" -> 83,
+    "two1nine"        -> 29,
+    "eightwothree"    -> 83,
     "abcone2threexyz" -> 13
   )
   def calibrationValue(line: String) =
     val digits = Map(
-      "one" -> "1",
-      "two" -> "2",
+      "one"   -> "1",
+      "two"   -> "2",
       "three" -> "3",
-      "four" -> "4",
-      "five" -> "5",
-      "six" -> "6",
+      "four"  -> "4",
+      "five"  -> "5",
+      "six"   -> "6",
       "seven" -> "7",
       "eight" -> "8",
-      "nine" -> "9"
+      "nine"  -> "9"
     )
 
     extension (m: String) def toDigit = digits.get(m).getOrElse(m)
 
     val digitPattern = (digits.keys ++ Seq("[0-9]")).mkString("|");
-    val firstDigit = digitPattern.r.findFirstIn(line).get.toDigit
+    val firstDigit   = digitPattern.r.findFirstIn(line).get.toDigit
     val lastDigit = s".*($digitPattern)".r
       .findFirstMatchIn(line)
       .get
