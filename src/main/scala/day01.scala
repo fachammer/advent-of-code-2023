883da8b5052bd42bfd293b3b77f430c94b26d9b4
Fabian Achammer <fabian@achammer.dev>
2023-12-01 08:54:53 +0100

    refactor part 2

--- src/main/scala/day01.scala
+++ src/main/scala/day01.scala
@@ -13,28 +13,26 @@ def sumOfCalibrationValuesWithDigitNames(input: String) =
   input.linesIterator.map(calibrationValueWithDigitNames).sum
 
 def calibrationValueWithDigitNames(line: String) =
-  extension (m: String)
-    def toDigit = m match
-      case "one"   => 1
-      case "two"   => 2
-      case "three" => 3
-      case "four"  => 4
-      case "five"  => 5
-      case "six"   => 6
-      case "seven" => 7
-      case "eight" => 8
-      case "nine"  => 9
-      case x       => x.toInt
+  val digits = Map(
+    "one" -> "1",
+    "two" -> "2",
+    "three" -> "3",
+    "four" -> "4",
+    "five" -> "5",
+    "six" -> "6",
+    "seven" -> "7",
+    "eight" -> "8",
+    "nine" -> "9"
+  )
 
-  val firstDigit = "(one|two|three|four|five|six|seven|eight|nine|[0-9])".r
-    .findFirstIn(line)
+  extension (m: String) def toDigit = digits.get(m).getOrElse(m)
+
+  val digitPattern = (digits.keys ++ Iterator("[0-9]")).mkString("|");
+  val firstDigit = digitPattern.r.findFirstIn(line).get.toDigit
+  val lastDigit = s".*($digitPattern)".r
+    .findFirstMatchIn(line)
     .get
+    .group(1)
     .toDigit
-  val lastDigit =
-    "(eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|[0-9])".r
-      .findFirstIn(line.reverse)
-      .get
-      .reverse
-      .toDigit
 
   s"$firstDigit$lastDigit".toInt
