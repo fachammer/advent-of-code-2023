9c2121e1b6dee63013f54921f677f86f2230d0a1
Fabian Achammer <fabian@achammer.dev>
2023-12-01 08:13:12 +0100

    refactor day01

--- src/main/scala/day01.scala
+++ src/main/scala/day01.scala
@@ -1,43 +1,40 @@
-def sumOfCalibrationValues(input: String): Int =
-  input.lines().mapToInt(calibrationValue).sum()
+def sumOfCalibrationValues(input: String) =
+  input.linesIterator.map(calibrationValue).sum
 
 def calibrationValue(line: String): Int =
-  val firstDigit =
-    line
-      .toCharArray()
-      .filter(Character.isDigit)
-      .map(Character.digit(_, 10))
-      .apply(0)
-  val lastDigit = line
+  val digits = line
     .toCharArray()
     .filter(Character.isDigit)
     .map(Character.digit(_, 10))
-    .lastOption
 
-  (firstDigit.toString() + lastDigit.get).toInt
+  s"${digits.head}${digits.last}".toInt
 
-def sumOfCalibrationValuesWithDigitNames(input: String): Int =
-  input.lines().mapToInt(calibrationValueWithDigitNames).sum()
+def sumOfCalibrationValuesWithDigitNames(input: String) =
+  input.linesIterator.map(calibrationValueWithDigitNames).sum
 
-def calibrationValueWithDigitNames(line: String): Int =
-  val digitPattern =
-    "(on(?=e)|tw(?=o)|thre(?=e)|four|fiv(?=e)|six|seve(?=n)|eigh(?=t)|nin(?=e)|[0-9])".r
-  val digits = digitPattern
-    .findAllIn(line)
-    .map(_ match {
-      case x if x.startsWith("on")   => 1
-      case x if x.startsWith("tw")   => 2
-      case x if x.startsWith("thre") => 3
-      case x if x.startsWith("four") => 4
-      case x if x.startsWith("fiv")  => 5
-      case x if x.startsWith("six")  => 6
-      case x if x.startsWith("seve") => 7
-      case x if x.startsWith("eigh") => 8
-      case x if x.startsWith("nin")  => 9
-      case x                         => x.toInt
-    })
-    .toArray
-  val firstDigit = digits.apply(0)
-  val lastDigit = digits.last
+def calibrationValueWithDigitNames(line: String) =
+  extension (m: String)
+    def toDigit = m match
+      case "one"   => 1
+      case "two"   => 2
+      case "three" => 3
+      case "four"  => 4
+      case "five"  => 5
+      case "six"   => 6
+      case "seven" => 7
+      case "eight" => 8
+      case "nine"  => 9
+      case x       => x.toInt
 
-  (firstDigit.toString() + lastDigit.toString()).toInt
+  val firstDigit = "(one|two|three|four|five|six|seven|eight|nine|[0-9])".r
+    .findFirstIn(line)
+    .get
+    .toDigit
+  val lastDigit =
+    "(eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|[0-9])".r
+      .findFirstIn(line.reverse)
+      .get
+      .reverse
+      .toDigit
+
+  s"$firstDigit$lastDigit".toInt
