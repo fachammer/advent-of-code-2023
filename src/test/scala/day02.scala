9d6dbd8e0a5d856831138b60f752eabee5f09c0d
Fabian Achammer <fabian@achammer.dev>
2023-12-02 07:48:56 +0100

    remove some day02 boilerplate

--- src/test/scala/day02.scala
+++ src/test/scala/day02.scala
@@ -1,7 +1,5 @@
 package day02
 
-class part1 extends test.DayPart(2):
-  def run(input: String) = ???
+class part1 extends test.DayPart(2)
 
-class part2 extends test.DayPart(2):
-  def run(input: String) = ???
+class part2 extends test.DayPart(2)
