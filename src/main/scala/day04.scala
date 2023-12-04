e45f8dbc88924cf05b36cfca6c4530ecc4aaa3ed
Fabian Achammer <fabian@achammer.dev>
2023-12-04 08:02:31 +0100

    solve day04 part 2

--- src/main/scala/day04.scala
+++ src/main/scala/day04.scala
@@ -2,17 +2,48 @@ package day04
 
 // part 1
 def points(input: String): Int =
-  input.linesIterator.map(parseCard).map(cardPoints.tupled).sum
+  input.linesIterator
+    .map(parseCard)
+    .map((_, winning, own) => cardPoints(winning, own))
+    .sum
 
-def parseCard(input: String): (Set[Int], Set[Int]) =
+def parseCard(input: String): (Int, Set[Int], Set[Int]) =
   input match
-    case s"$card: $winningNumbers | $ownNumbers" =>
+    case s"Card $card: $winningNumbers | $ownNumbers" =>
       (
-        winningNumbers.split(" ").filterNot(_.isEmpty).map(_.toInt).toSet,
-        ownNumbers.split(" ").filterNot(_.isBlank).map(_.toInt).toSet
+        card.strip.toInt,
+        winningNumbers
+          .split(" ")
+          .filterNot(_.isEmpty)
+          .map(_.strip)
+          .map(_.toInt)
+          .toSet,
+        ownNumbers
+          .split(" ")
+          .filterNot(_.isBlank)
+          .map(_.strip)
+          .map(_.toInt)
+          .toSet
       )
 
 def cardPoints(winningNumbers: Set[Int], ourNumbers: Set[Int]): Int =
   winningNumbers.intersect(ourNumbers).size match
     case 0 => 0
     case n => scala.math.pow(2, n - 1).toInt
+
+// part2
+def numberOfScratchCards(input: String): Int =
+  val numberOfCards = input.linesIterator.length
+  var cardAmounts =
+    scala.collection.mutable.Map[Int, Int]((1 to numberOfCards).map((_, 1))*)
+
+  for line <- input.linesIterator do
+    val (card, winningNumbers, ownNumbers) = parseCard(line)
+    val intersection    = winningNumbers.intersect(ownNumbers)
+    val numberOfMatches = intersection.size
+    for
+      i <- (card + 1) to (card + numberOfMatches)
+      if i <= numberOfCards
+    do cardAmounts(i) += cardAmounts(card)
+
+  cardAmounts.values.sum
--- src/test/scala/day04.scala
+++ src/test/scala/day04.scala
@@ -4,5 +4,6 @@ import test.*
 
 class Day04 extends DayTest(4):
   def parts = Seq(
-    Part(points, "example" -> 13, "input" -> 32609)
+    Part(points, "example"               -> 13, "input" -> 32609),
+    Part(numberOfScratchCards, "example" -> 30, "input" -> 14624680)
   )
