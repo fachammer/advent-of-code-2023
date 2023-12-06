cebe352ea92c14a69d0e51698d478ec9369836fc
Fabian Achammer <fabian@achammer.dev>
2023-12-06 23:31:49 +0100

    refactor day05

--- src/main/scala/day06.scala
+++ src/main/scala/day06.scala
@@ -1,87 +1,41 @@
 package day06
 
-import java.math.MathContext
-import java.math.RoundingMode
-
 // part 1
 def productOfNumberOfWaysToBeatRecord(input: String) =
-  val List(s"Time: $timesString", s"Distance: $distanceString") =
-    input.linesIterator.toSeq: @unchecked
+  val timePattern     = "Time: +(.*)".r
+  val distancePattern = "Distance: +(.*)".r
+  val Seq(
+    timePattern(durationsString),
+    distancePattern(recordDistancesString)
+  ) = input.linesIterator.toSeq: @unchecked
 
-  val times =
-    timesString.split(" +").map(_.strip).filterNot(_.isEmpty).map(_.toInt)
-  val distances =
-    distanceString.split(" +").map(_.strip).filterNot(_.isEmpty).map(_.toInt)
-  val races = times.zip(distances)
+  val raceDurations   = durationsString.split(" +").map(_.toLong)
+  val recordDistances = recordDistancesString.split(" +").map(_.toLong)
+  val races           = raceDurations.zip(recordDistances)
 
-  // let t_r be the duration of the race
-  // let t_s be the duration of holding the button
-  // let d(t_s, t_r) be the distance travelled after t_r seconds when holding the button for t_s seconds
-  // d(t_s) = t_s(t_r-t_s)
-  // let r be the record distance
-  // we want to find all the choices for t_s such that d(t_s, t_r) > r
-  // i.e. all t_d such that t_s (t_r - t_s) - r > 0
-  // we look for the roots of -t_s^2 + t_r t_s -r
-  // those are (-1 / 2) (-t_r +- sqrt(t_r^2 - 4r))
+  races.map(numberOfWaysToBeatRecord.tupled).product
 
-  def roots(raceDuration: Int, recordDistance: Int): (Double, Double) =
-    (
-      (-1.0 / 2) * (-raceDuration + scala.math.sqrt(
-        raceDuration * raceDuration - 4 * recordDistance
-      )),
-      (-1.0 / 2) * (-raceDuration - scala.math.sqrt(
-        raceDuration * raceDuration - 4 * recordDistance
-      ))
-    )
-
-  races
-    .map(roots.tupled)
-    .map((left, right) => (left.nextLong, right.previousLong))
-    .map((left, right) => {
-      assert(right >= left)
-      right - left + 1
-    })
-    .product
+def numberOfWaysToBeatRecord(raceDuration: Long, recordDistance: Long): Long =
+  val discriminant     = raceDuration * raceDuration - 4 * recordDistance
+  val discriminantRoot = scala.math.sqrt(discriminant)
+  val leftRoot         = 0.5 * (raceDuration - discriminantRoot)
+  val rightRoot        = 0.5 * (raceDuration + discriminantRoot)
+  rightRoot.previousLong - leftRoot.nextLong + 1
 
 // part 2
-def numberOfWaysToBeatRecord(input: String) =
-  val List(s"Time: $timesString", s"Distance: $distanceString") =
+def numberOfWaysToBeatRecord(input: String): Long =
+  val Seq(s"Time: $timesString", s"Distance: $distanceString") =
     input.linesIterator.toSeq: @unchecked
 
-  val time     = timesString.replaceAll(" ", "").toLong
-  val distance = distanceString.replaceAll(" ", "").toLong
+  val raceDuration   = timesString.replaceAll(" ", "").toLong
+  val recordDistance = distanceString.replaceAll(" ", "").toLong
 
-  def bigDecimalRoots(
-      raceDuration: Long,
-      recordDistance: Long
-  ): (BigDecimal, BigDecimal) =
-    (
-      BigDecimal(-1.0 / 2) * (BigDecimal(-raceDuration) + BigDecimal(
-        raceDuration * raceDuration - 4 * recordDistance
-      ).bigDecimal.sqrt(MathContext.DECIMAL128)),
-      BigDecimal(-1.0 / 2) * (BigDecimal(-raceDuration) - BigDecimal(
-        raceDuration * raceDuration - 4 * recordDistance
-      ).bigDecimal.sqrt(MathContext.DECIMAL128))
-    )
+  numberOfWaysToBeatRecord(raceDuration, recordDistance)
 
-  val (left, right) = bigDecimalRoots(time, distance)
-
-  right.previousLong - left.nextLong + 1
-
-extension (number: BigDecimal)
+extension (number: Double)
   def nextLong =
-    if number.isValidLong then number.longValue + 1
-    else
-      val digitsBeforeDecimalPoint =
-        number.bigDecimal.toString.split(raw"\.").head.length
-      number
-        .round(MathContext(digitsBeforeDecimalPoint, RoundingMode.CEILING))
-        .longValue
+    if number.isValidInt then number.longValue + 1
+    else number.ceil.toLong
   def previousLong =
-    if number.isValidLong then number.longValue - 1
-    else
-      val digitsBeforeDecimalPoint =
-        number.bigDecimal.toString.split(raw"\.").head.length
-      number
-        .round(MathContext(digitsBeforeDecimalPoint, RoundingMode.FLOOR))
-        .longValue
+    if number.isValidInt then number.longValue - 1
+    else number.floor.toLong
