93daa47ff8c32d16246ddcffd1c24acc3baa594e
Fabian Achammer <fabian@achammer.dev>
2023-12-07 08:01:42 +0100

    solve day07 part 2

--- src/main/scala/day07.scala
+++ src/main/scala/day07.scala
@@ -1,6 +1,5 @@
 package day07
 
-import debug._
 import scala.math.Ordered.orderingToOrdered
 
 // part 1
@@ -18,17 +17,19 @@ case class Hand(values: Seq[Int]):
     val groups = values.groupBy(x => x)
     if groups.size == 1 then HandType.FiveOfAKind
     else if groups.size == 2 then
-      if groups.values.map(_.size).toSet == Set(1, 4) then HandType.FourOfAKind
+      if groups.values.map(_.size).toSeq.sorted.reverse == Seq(4, 1) then
+        HandType.FourOfAKind
       else HandType.FullHouse
     else if groups.size == 3 then
-      if groups.values.map(_.size).toSet == Set(3, 1, 1) then
+      if groups.values.map(_.size).toSeq.sorted.reverse == Seq(3, 1, 1) then
         HandType.ThreeOfAKind
       else HandType.TwoPair
     else if groups.size == 4 then HandType.OnePair
     else HandType.HighCard
 
-object HandOrdering extends Ordering[Hand]:
-  override def compare(left: Hand, right: Hand): Int =
+class HandOrdering(using handTypeOrdering: Ordering[HandType])
+    extends Ordering[Hand]:
+  def compare(left: Hand, right: Hand): Int =
     val comparison = left.handType.compare(right.handType)
     if comparison != 0 then comparison
     else right.values.compare(left.values)
@@ -37,22 +38,17 @@ object HandTypeOrdering extends Ordering[HandType]:
   override def compare(left: HandType, right: HandType): Int =
     left.ordinal.compare(right.ordinal)
 
-given Ordering[Hand]     = HandOrdering
-given Ordering[HandType] = HandTypeOrdering
-
 def totalWinnings(input: String) =
   val bids = input.linesIterator
     .map(_.span(x => !x.isSpaceChar))
     .map((hand, bid) => (Hand(hand.map(cardValue)), bid.strip.toInt))
     .toSeq
 
-  bids.sorted.reverse.zipWithIndex
-    .map { case ((hand, bid), index) =>
-      (hand, hand.handType, bid * (index + 1))
-    }
-    .d
-    .map((_, _, winning) => winning)
-    .sum
+  given Ordering[HandType] = HandTypeOrdering
+  given Ordering[Hand]     = HandOrdering()
+  bids.sorted.reverse.zipWithIndex.map { case ((_, bid), index) =>
+    bid * (index + 1)
+  }.sum
 
 def cardValue(char: Char): Int = char match
   case 'A'            => 14
@@ -61,3 +57,53 @@ def cardValue(char: Char): Int = char match
   case 'J'            => 11
   case 'T'            => 10
   case x if x.isDigit => x.asDigit
+
+// part 2
+case class HandWithJoker(hand: String):
+  def handType: HandType =
+    val groupsTypeWithoutJoker = hand
+      .groupBy(x => x)
+      .filterKeys(_ != 'J')
+      .values
+      .map(_.size)
+      .toSeq
+      .sorted
+      .reverse
+
+    val numberOfJokers = hand.groupBy(x => x).get('J').map(_.size).getOrElse(0)
+    import HandType.*
+    (groupsTypeWithoutJoker, numberOfJokers) match
+      case (_, 5) | (_, 4) | (Seq(2), 3) | (Seq(3), 2) | (Seq(4), 1) =>
+        FiveOfAKind
+      case (Seq(1, 1), 3) | (Seq(2, 1), 2) | (Seq(3, 1), 1) => FourOfAKind
+      case (Seq(2, 2), 1)                                   => FullHouse
+      case (Seq(1, 1, 1), 2) | (Seq(2, 1, 1), 1)            => ThreeOfAKind
+      case (Seq(1, 1, 1, 1), 1)                             => OnePair
+      case (_, 0) => Hand(hand.map(cardValue)).handType
+
+class HandWithJokerOrdering(using handTypeOrdering: Ordering[HandType])
+    extends Ordering[HandWithJoker]:
+  def compare(left: HandWithJoker, right: HandWithJoker): Int =
+    val comparison = left.handType.compare(right.handType)
+    def cardValueWithJoker(char: Char) =
+      if char == 'J' then 1 else cardValue(char)
+    if comparison != 0 then comparison
+    else
+      right.hand
+        .map(cardValueWithJoker)
+        .compare(left.hand.map(cardValueWithJoker))
+
+def totalWinningsWithJoker(input: String) =
+  val bids = input.linesIterator
+    .map(_.span(x => !x.isSpaceChar))
+    .map((hand, bid) => (HandWithJoker(hand), bid.strip.toInt))
+    .toSeq
+
+  given Ordering[HandType]      = HandTypeOrdering
+  given Ordering[HandWithJoker] = HandWithJokerOrdering()
+  bids.sorted.reverse.zipWithIndex
+    .map { case ((hand, bid), index) =>
+      (hand, hand.handType, index + 1, bid, bid * (index + 1))
+    }
+    .map((_, _, _, _, winning) => winning)
+    .sum
--- src/test/scala/day07.scala
+++ src/test/scala/day07.scala
@@ -1,9 +1,6 @@
 class Day07 extends DayTest(7):
   import day07.*
   def parts = Seq(
-    Part(
-      totalWinnings,
-      "example" -> 6440,
-      "input"   -> 0
-    )
+    Part(totalWinnings, "example"          -> 6440, "input" -> 241344943),
+    Part(totalWinningsWithJoker, "example" -> 5905, "input" -> 243101568)
   )
