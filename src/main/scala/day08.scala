94abc6658a383cb9c717ad00ebf91b54924cc46f
Fabian Achammer <fabian@achammer.dev>
2023-12-08 08:42:33 +0100

    solve day08 part 2

--- src/main/scala/day08.scala
+++ src/main/scala/day08.scala
@@ -20,7 +20,60 @@ def requiredSteps(input: String) =
       case (node, 'R') => nodes(node)._2
     }
     .takeWhile(_ != "ZZZ")
-    .d
     .length
-  // .scanLeft("AAA")(x => "X")
-  // .d
+
+// part 2
+def requiredStepsAsGhost(input: String) =
+  val steps = input.linesIterator.next.d
+  steps.length.d
+  val nodes = input.linesIterator
+    .drop(2)
+    .map(line =>
+      val s"$node = ($leftNode, $rightNode)" = line: @unchecked
+      (node, (leftNode, rightNode))
+    )
+    .toMap
+
+  def requiredStepsToAnEndNode(startNode: String, stepOffset: Int) =
+    val stepsIter = LazyList
+      .concat(LazyList.continually(steps.toSeq.zipWithIndex)*)
+      .toIterator
+      .drop(stepOffset)
+    var node       = startNode.d
+    var count      = 0
+    var stepsIndex = stepOffset
+    while count == 0 || !node.endsWith("Z") do
+      assert(node != "XXX")
+      count += 1
+      val (step, index) = stepsIter.next
+      stepsIndex = index
+      node = step match
+        case 'L' => nodes(node)._1
+        case 'R' => nodes(node)._2
+
+    (count, node, stepsIndex)
+
+  def gcd(a: Long, b: Long): Long = if (b == 0) a else gcd(b, a % b)
+  def lcm(a: Long, b: Long)       = (a * b) / gcd(a, b)
+
+  val startNodes = nodes.filterKeys(_.endsWith("A")).keys
+  val requiredSteps = lcm(
+    startNodes
+      .map { startNode =>
+        val (phase, endNode, stepsIndex) =
+          requiredStepsToAnEndNode(startNode, 0).d
+        val (cycleLength, _, endNodeStepsIndex) =
+          requiredStepsToAnEndNode(endNode, stepsIndex + 1).d
+        (
+          startNode,
+          phase,
+          stepsIndex,
+          endNode,
+          cycleLength,
+          endNodeStepsIndex
+        )
+      }
+      .foldLeft(1L) { case (a, (_, _, _, _, b, _)) => lcm(a.toLong, b.toLong) },
+    steps.length
+  )
+  requiredSteps.d
--- src/test/scala/day08.scala
+++ src/test/scala/day08.scala
@@ -1,5 +1,6 @@
 class Day08 extends DayTest(8):
   import day08.*
   def parts = Seq(
-    Part(requiredSteps, "example1" -> 2, "example2" -> 6, "input" -> 20093)
+    Part(requiredSteps, "example1" -> 2, "example2" -> 6, "input" -> 20093),
+    Part(requiredStepsAsGhost, "example3" -> 6, "input" -> 22103062509257L)
   )
