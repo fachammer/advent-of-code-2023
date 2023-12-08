854d4bc28af287c41707c139b7fdad96793185eb
Fabian Achammer <fabian@achammer.dev>
2023-12-08 11:55:05 +0100

    refactor day08

--- src/main/scala/day08.scala
+++ src/main/scala/day08.scala
@@ -1,79 +1,75 @@
 package day08
 
-import debug.*
-
 // part 1
+type DesertMap = Map[String, (String, String)]
 def requiredSteps(input: String) =
-  val steps = input.linesIterator.next
-  val nodes = input.linesIterator
-    .drop(2)
-    .map(line =>
-      val s"$node = ($leftNode, $rightNode)" = line: @unchecked
-      (node, (leftNode, rightNode))
-    )
-    .toMap
+  val (steps, nodes) = parseInput(input)
+  given DesertMap    = nodes
 
-  LazyList
-    .concat(LazyList.continually(steps.toSeq)*)
-    .scanLeft("AAA") {
-      case (node, 'L') => nodes(node)._1
-      case (node, 'R') => nodes(node)._2
-    }
-    .takeWhile(_ != "ZZZ")
-    .length
+  val (length, _, _) = accumulate((0, "AAA", repeat(steps))):
+    (length, node, stepsIter) =>
+      Option.when(node != "ZZZ"):
+        (length + 1, nextNode(node, stepsIter.next), stepsIter)
+
+  length
 
 // part 2
 def requiredStepsAsGhost(input: String) =
-  val steps = input.linesIterator.next.d
-  steps.length.d
-  val nodes = input.linesIterator
-    .drop(2)
-    .map(line =>
-      val s"$node = ($leftNode, $rightNode)" = line: @unchecked
-      (node, (leftNode, rightNode))
-    )
-    .toMap
+  val (steps, nodes) = parseInput(input)
+  given DesertMap    = nodes
 
-  def requiredStepsToAnEndNode(startNode: String, stepOffset: Int) =
-    val stepsIter = LazyList
-      .concat(LazyList.continually(steps.toSeq.zipWithIndex)*)
-      .toIterator
-      .drop(stepOffset)
-    var node       = startNode.d
-    var count      = 0
-    var stepsIndex = stepOffset
-    while count == 0 || !node.endsWith("Z") do
-      assert(node != "XXX")
-      count += 1
-      val (step, index) = stepsIter.next
-      stepsIndex = index
-      node = step match
-        case 'L' => nodes(node)._1
-        case 'R' => nodes(node)._2
+  case class FindResult(val end: String, val pathLength: Long, val offset: Int)
+  def findEndNode(startNode: String, stepOffset: Int) =
+    val (end, length, iter) = {
+      accumulate((startNode, 0L, repeat(steps.zipWithIndex))):
+        case (node, lengthFromStart, stepsWithIndexIter) =>
+          Option.when(lengthFromStart == 0 || !node.endsWith("Z")):
+            val (step, _) = stepsWithIndexIter.next
+            (nextNode(node, step), lengthFromStart + 1, stepsWithIndexIter)
+    }
+    FindResult(end, length, iter.next._2)
 
-    (count, node, stepsIndex)
+  def cycleLengthStartingFrom(startNode: String) =
+    val FindResult(endNode, length, stepsIndex) = findEndNode(startNode, 0)
+    assertAllEndNodeCyclesHaveLength(endNode, stepsIndex, length)
+    // given the above assertion we know that the cycle length is the same as the initial path length from start to end
+    length
+
+  def assertAllEndNodeCyclesHaveLength(end: String, offset: Int, length: Long) =
+    accumulate((0L, offset)) { (count, stepOffset) =>
+      Option.when(count == 0 || stepOffset != offset):
+        val foundNode = findEndNode(end, stepOffset)
+        // we assume that there is a unique end node on every cycle
+        assert(foundNode.end == end)
+        // and that the path lengths from end node to end node are the same regardless of where we are in the step sequence
+        assert(foundNode.pathLength == length)
+        (count + 1, foundNode.offset)
+    }
+    true
 
   def gcd(a: Long, b: Long): Long = if (b == 0) a else gcd(b, a % b)
   def lcm(a: Long, b: Long)       = (a * b) / gcd(a, b)
 
   val startNodes = nodes.filterKeys(_.endsWith("A")).keys
-  val requiredSteps = lcm(
-    startNodes
-      .map { startNode =>
-        val (phase, endNode, stepsIndex) =
-          requiredStepsToAnEndNode(startNode, 0).d
-        val (cycleLength, _, endNodeStepsIndex) =
-          requiredStepsToAnEndNode(endNode, stepsIndex + 1).d
-        (
-          startNode,
-          phase,
-          stepsIndex,
-          endNode,
-          cycleLength,
-          endNodeStepsIndex
-        )
-      }
-      .foldLeft(1L) { case (a, (_, _, _, _, b, _)) => lcm(a.toLong, b.toLong) },
-    steps.length
-  )
-  requiredSteps.d
+  startNodes.map(cycleLengthStartingFrom).reduceOption(lcm)
+
+def nextNode(node: String, step: Char)(using nodes: DesertMap) = step match
+  case 'L' => nodes(node)._1
+  case 'R' => nodes(node)._2
+
+def parseInput(input: String): (String, Map[String, (String, String)]) =
+  val steps = input.linesIterator.next
+  def parseLine(line: String) = line match
+    case s"$node = ($left, $right)" => (node, (left, right))
+  val nodes = input.linesIterator.drop(2).map(parseLine).toMap
+  (steps, nodes)
+
+def repeat[A](seq: Seq[A]) = LazyList.continually(seq).flatten.toIterator
+
+def accumulate[S](initial: S)(op: S => Option[S]): S =
+  var a = initial
+  while true do
+    op(a) match
+      case None    => return a
+      case Some(b) => a = b
+  ???
--- src/test/scala/day08.scala
+++ src/test/scala/day08.scala
@@ -2,5 +2,9 @@ class Day08 extends DayTest(8):
   import day08.*
   def parts = Seq(
     Part(requiredSteps, "example1" -> 2, "example2" -> 6, "input" -> 20093),
-    Part(requiredStepsAsGhost, "example3" -> 6, "input" -> 22103062509257L)
+    Part(
+      requiredStepsAsGhost,
+      "example3" -> Some(6),
+      "input"    -> Some(22103062509257L)
+    )
   )
