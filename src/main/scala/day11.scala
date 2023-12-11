e472871330f4ac0fb5506b433e2b71e275c50751
Fabian Achammer <fabian@achammer.dev>
2023-12-11 07:19:37 +0100

    solve day11 part 2

--- src/main/scala/day11.scala
+++ src/main/scala/day11.scala
@@ -2,24 +2,23 @@ package day11
 
 // part 1
 def sumOfShortestPaths(input: String) =
-  val universe = expandedUniverse(input)
-  val galaxyPositions =
-    for
-      (line, row) <- universe.zipWithIndex
-      case ('#', col) <- line.zipWithIndex
-    yield (col, row)
+  sumOfShortestPathsWithExpansion(2)(input)
+
+// part 2
+def sumOfShortestPathsWithExpansion(expansion: Int)(input: String) =
+  val galaxyPositions = (for
+    (line, row) <- input.linesIterator.zipWithIndex
+    case ('#', col) <- line.zipWithIndex
+  yield (col, row)).toSeq
+  val universe           = input.linesIterator.toSeq
+  val transposedUniverse = universe.transpose
 
   galaxyPositions.combinations(2).map { case Seq((colA, rowA), (colB, rowB)) =>
-    (colB - colA).abs + (rowB - rowA)
+    val colDistance = ((colA.min(colB) + 1) to colA.max(colB)).map(col =>
+      if transposedUniverse(col).forall(_ == '.') then expansion else 1,
+    ).sum.toLong
+    val rowDistance = ((rowA.min(rowB) + 1) to rowA.max(rowB))
+      .map(row => if universe(row).forall(_ == '.') then expansion else 1).sum
+      .toLong
+    colDistance + rowDistance
   }.sum
-
-def expandedUniverse(input: String) =
-  val lines = input.linesIterator.toSeq
-  val cols  = lines.transpose
-
-  val expandedCols = cols.flatMap(column =>
-    if column.forall(_ == '.') then Seq(column, column) else Seq(column),
-  )
-  expandedCols.transpose.flatMap(line =>
-    if line.forall(_ == '.') then Seq(line, line) else Seq(line),
-  )
--- src/test/scala/day11.scala
+++ src/test/scala/day11.scala
@@ -6,4 +6,11 @@ import day11.*
 given Day = Day(11)
 class Test extends DayTest:
   def parts =
-    Seq(Part(sumOfShortestPaths, "example" -> 374, "input" -> 9536038))
+    Seq(
+      Part(sumOfShortestPaths, "example" -> 374, "input" -> 9536038),
+      Part(sumOfShortestPathsWithExpansion(10), "example"         -> 1030),
+      Part(sumOfShortestPathsWithExpansion(100), "example"        -> 8410),
+      Part(sumOfShortestPathsWithExpansion(1), "example2"         -> 10),
+      Part(sumOfShortestPathsWithExpansion(1_000_000), "example2" -> 8_000_002),
+      Part(sumOfShortestPathsWithExpansion(1_000_000), "input" -> 447744640566L),
+    )
