08d8699296a0b89847162a5f1ea1910dcb854c60
Fabian Achammer <fabian@achammer.dev>
2023-12-12 10:31:29 +0100

    implement very inefficient part 2

--- src/main/scala/day12.scala
+++ src/main/scala/day12.scala
@@ -1,26 +1,99 @@
 package day12
 
+import scala.util.Try
+
 // part 1
 def sumOfSpringConfigurations(input: String) =
   input.linesIterator.map(numberOfSpringConfigurations).sum
 
+def processDamagesFromFront(damageGroupSizes: Array[Int], s: String) =
+  import scala.collection.mutable
+  val processedString = mutable.StringBuilder(s)
+  def canProcess =
+    val solvableIndex = processedString.indexOf("#?")
+    solvableIndex != -1 && solvableIndex < processedString.indexOf("?")
+  while canProcess do
+    val firstSolvableIndex = processedString.indexOf("#?")
+    val frontKnown        = processedString.substring(0, firstSolvableIndex + 1)
+    val frontDamageGroups = frontKnown.split(raw"\.+").filter(_.nonEmpty)
+    val frontGroupIter    = frontDamageGroups.dropRight(1).iterator
+    val damageGroupIter   = damageGroupSizes.iterator
+    while frontGroupIter.hasNext do
+      val (d, len) = (frontGroupIter.next, damageGroupIter.next)
+      if d.length != len then
+        throw Exception("unsatisfiable: known group sizes don't match")
+
+    def startIndex =
+      val i = processedString.substring(0, firstSolvableIndex + 1).reverse
+        .indexWhere(_ != '#')
+      if i == -1 then 0 else firstSolvableIndex + 1 - i
+    val start = startIndex
+    var i     = start
+    if !damageGroupIter.hasNext then
+      throw Exception("unsatisfiable: too many groups")
+    val groupSize = damageGroupIter.next
+    while i - start < groupSize do
+      if i >= processedString.length then
+        throw Exception("unsatisfiable: last group too small")
+      if processedString(i) == '.' then
+        throw Exception(
+          "unsatisfiable: encountered . when expecting unknown or #",
+        )
+      processedString(i) = '#'
+      i += 1
+
+    if i < processedString.length then
+      if processedString(i) == '#' then
+        throw Exception("unsatisfiable: group too large")
+      else processedString(i) = '.'
+
+  processedString.mkString
+
+def processDamagesFromBack(damageGroupSizes: Array[Int], s: String) =
+  processDamagesFromFront(damageGroupSizes.reverse, s.reverse).reverse
+
+def processDamages(damageGroupSizes: Array[Int], s: String): String =
+  processDamagesFromBack(
+    damageGroupSizes,
+    processDamagesFromFront(damageGroupSizes, s),
+  )
+
 def numberOfSpringConfigurations(line: String) =
-  val s"$springs $constraints" = line: @unchecked
-  val damageGroupSizes         = constraints.split(',').map(_.toInt)
+  val (springs, damageGroupSizes) = parseLine(line)
 
   import scala.collection.mutable
-  val queue     = mutable.Queue(springs)
+  val stack     = mutable.Stack(springs)
   val satisfied = mutable.Stack[String]()
-  def isValid(springs: String) =
-    springs.split(raw"\.+").map(_.length).filter(_ != 0)
-      .sameElements(damageGroupSizes)
 
-  while !queue.isEmpty do
-    val s = queue.dequeue()
-    if s.contains('?') then
-      val withWorking = s.replaceFirst(raw"\?", ".")
-      val withDamaged = s.replaceFirst(raw"\?", "#")
-      queue.enqueueAll(Seq(withWorking, withDamaged))
-    else if isValid(s) then satisfied.push(s)
+  def isDefinitelyValid(springs: String) =
+    !springs.contains("?") && springs.split(raw"\.+").map(_.length)
+      .filter(_ != 0).sameElements(damageGroupSizes)
+
+  while !stack.isEmpty do
+    val s = Try(processDamages(damageGroupSizes, stack.pop))
+    if !s.isFailure then
+      s.get match
+        case s if s.contains('?') =>
+          val withWorking = s.replaceFirst(raw"\?", ".")
+          val withDamaged = s.replaceFirst(raw"\?", "#")
+          stack.push(withWorking, withDamaged)
+        case s if isDefinitelyValid(s) => satisfied.push(s)
+        case _                         =>
 
   satisfied.length
+
+def parseLine(line: String) =
+  val s"$springs $constraints" = line: @unchecked
+  val damageGroupSizes         = constraints.split(',').map(_.toInt)
+  (springs, damageGroupSizes)
+
+// part 2
+def sumOfUnfoldedSpringConfigurations(input: String) =
+  input.linesIterator.zipWithIndex
+    .tapEach((line, i) => println(s"at line $i: $line"))
+    .map((line, _) => unfold(line)).map(numberOfSpringConfigurations).sum
+
+def unfold(line: String): String =
+  val s"$springs $constraints" = line: @unchecked
+  s"${Iterator.continually(springs).take(5).mkString("?")} ${Iterator
+      .continually(constraints).take(5).mkString(",")}"
--- src/test/scala/day12.scala
+++ src/test/scala/day12.scala
@@ -4,13 +4,30 @@ import day12.*
 given Day = Day(12)
 class Test extends DayTest:
   def parts =
-    Seq(Part(sumOfSpringConfigurations, "example" -> 21, "input" -> 8270))
+    Seq(
+      Part(sumOfSpringConfigurations, "example" -> 21, "input" -> 8270),
+      Part(
+        sumOfUnfoldedSpringConfigurations,
+        "example" -> 525152,
+        // "input" -> 0
+      ),
+    )
 
   "number of spring configurations" - numberOfSpringConfigurations.testCases(
-    "a" :> "???.### 1,1,3"             -> 1,
-    "b" :> ".??..??...?##. 1,1,3"      -> 4,
-    "c" :> "?#?#?#?#?#?#?#? 1,3,1,6"   -> 1,
-    "d" :> "????.#...#... 4,1,1"       -> 1,
-    "e" :> "????.######..#####. 1,6,5" -> 4,
-    "f" :> "?###???????? 3,2,1"        -> 10,
+    "a" :> "???.### 1,1,3"                             -> 1,
+    "b" :> ".??..??...?##. 1,1,3"                      -> 4,
+    "c" :> "?#?#?#?#?#?#?#? 1,3,1,6"                   -> 1,
+    "d" :> "????.#...#... 4,1,1"                       -> 1,
+    "e" :> "????.######..#####. 1,6,5"                 -> 4,
+    "f" :> "?###???????? 3,2,1"                        -> 10,
+    "a unfold" :> (unfold("???.### 1,1,3")             -> 1),
+    "b unfold" :> (unfold(".??..??...?##. 1,1,3")      -> 16384),
+    "c unfold" :> (unfold("?#?#?#?#?#?#?#? 1,3,1,6")   -> 1),
+    "d unfold" :> (unfold("????.#...#... 4,1,1")       -> 16),
+    "e unfold" :> (unfold("????.######..#####. 1,6,5") -> 2500),
+    "f unfold" :> (unfold("?###???????? 3,2,1")        -> 506250),
   )
+
+  "process damages" -
+    processDamages.tupled
+      .testCases("b" :> (Array(1, 1, 3), ".??..??...?##.") -> ".??..??...###.")
