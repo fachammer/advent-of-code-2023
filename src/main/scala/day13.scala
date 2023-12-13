59ac4f96eb96046ed881e55326934f23a407e585
Fabian Achammer <fabian@achammer.dev>
2023-12-13 09:24:59 +0100

    solve day13 part 2

--- src/main/scala/day13.scala
+++ src/main/scala/day13.scala
@@ -1,26 +1,52 @@
 package day13
 
-import debug.*
-
 // part 1
-def summarize(input: String) =
-  val patterns = input.split("\n\n")
-    .map(_.linesIterator.map(_.toVector).toVector)
-  patterns.map(reflectionCount).sum
+enum Reflection:
+  case Horizontal(val row: Int)
+  case Vertical(val col: Int)
+type Pattern = Vector[Vector[Char]]
+def summarize(input: String) = patterns(input).map(reflectionCount).sum
 
-def reflectionCount(pattern: Vector[Vector[Char]]): Int =
-  verticalReflectionColumn(pattern)
-    .orElse(horizontalReflectionRow(pattern).map(_ * 100)).get
+def patterns(input: String) =
+  input.split("\n\n").map(_.linesIterator.map(_.toVector).toVector)
+def reflectionCount(pattern: Pattern): Int =
+  reflections(pattern).headOption.map(reflectionValue).getOrElse(0)
 
-def verticalReflectionColumn(pattern: Vector[Vector[Char]]): Option[Int] =
-  horizontalReflectionRow(pattern.transpose)
+def reflectionValue(r: Reflection): Int =
+  r match
+    case Reflection.Horizontal(row) => row * 100
+    case Reflection.Vertical(col)   => col
 
-def horizontalReflectionRow(pattern: Vector[Vector[Char]]): Option[Int] =
-  val result = (1 to pattern.length / 2).find { i =>
-    pattern.take(i).sameElements(pattern.drop(i).take(i).reverse)
+def reflections(pattern: Pattern): Seq[Reflection] =
+  horizontalReflectionRows(pattern).map(Reflection.Horizontal(_)) ++
+    verticalReflectionColumns(pattern).map(Reflection.Vertical(_))
+
+def verticalReflectionColumns(pattern: Pattern): Seq[Int] =
+  horizontalReflectionRows(pattern.transpose)
+
+def horizontalReflectionRows(pattern: Pattern): Seq[Int] =
+  def forwardReflectionRow(p: Pattern) = (1 to p.length / 2).find { i =>
+    p.take(i).sameElements(p.drop(i).take(i).reverse)
   }
-  val reversed = pattern.reverse
-  val reverseResult = (1 to reversed.length / 2).find { i =>
-    reversed.take(i).sameElements(reversed.drop(i).take(i).reverse)
-  }.map(pattern.length - _)
-  result.orElse(reverseResult)
+
+  val result  = forwardReflectionRow(pattern)
+  val reverse = forwardReflectionRow(pattern.reverse).map(pattern.length - _)
+  Seq(result, reverse).flatten
+
+// part 2
+def summarizeWithSmudgeRemoved(input: String) =
+  patterns(input).map(reflectionWithSmudgeRemoved).map(reflectionValue).sum
+
+def reflectionWithSmudgeRemoved(pattern: Pattern): Reflection =
+  val reflection = reflections(pattern).head
+  val differentReflections =
+    for
+      row <- 0 until pattern.length
+      col <- 0 until pattern(0).length
+      r   <- reflections(withoutSmudgeAt(pattern, row, col)) if r != reflection
+    yield r
+  differentReflections.head
+
+def withoutSmudgeAt(pattern: Pattern, row: Int, col: Int): Pattern =
+  val opposite = if pattern(row)(col) == '.' then '#' else '.'
+  pattern.updated(row, pattern(row).updated(col, opposite))
--- src/test/scala/day13.scala
+++ src/test/scala/day13.scala
@@ -3,4 +3,13 @@ import test.*
 import day13.*
 given Day = Day(13)
 class Test extends DayTest:
-  def parts = Seq(Part(summarize, "example" -> 405, "input" -> 0))
+  def parts =
+    Seq(
+      Part(summarize, "example"                  -> 405, "input" -> 27202),
+      Part(summarizeWithSmudgeRemoved, "example" -> 400, "input" -> 41566),
+    )
+
+  reflectionWithSmudgeRemoved.testCases(
+    "a" :> (patterns(file("example"))(1) -> Reflection.Horizontal(1)),
+    "b" :> (patterns(file("input"))(0)   -> Reflection.Vertical(16)),
+  )
