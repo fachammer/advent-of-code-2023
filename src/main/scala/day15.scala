ca10cc2c028cf55ce5b711f6d0b886f9cfbd63dd
Fabian Achammer <fabian@achammer.dev>
2023-12-15 08:30:44 +0100

    refactor day15

--- src/main/scala/day15.scala
+++ src/main/scala/day15.scala
@@ -4,41 +4,20 @@ package day15
 def hashValuesSum(input: String) = input.strip.split(',').map(hashValue).sum
 
 def hashValue(input: String) =
-  input
-    .map(_.toInt)
-    .fold(0): (current, next) =>
-      ((current + next) * 17) % 256
+  input.map(_.toInt).fold(0)((current, next) => ((current + next) * 17) % 256)
 
 // part 2
-def focusingPowerSum(input: String) =
-  initialize(input).zipWithIndex
-    .map((box, boxNumber) =>
-      box.zipWithIndex.map { case ((_, focalLength), slotIndex) =>
-        (boxNumber + 1) * (slotIndex + 1) * focalLength
-      }.sum,
-    )
-    .sum
-
-def initialize(input: String): Array[Vector[(String, Int)]] =
+def focusingPower(input: String) =
+  import scala.collection.mutable.LinkedHashMap
+  val boxes = Array.fill(256)(LinkedHashMap.empty[String, Int])
   val steps = input.strip.split(',')
-  val boxes = Array.fill(256)(Vector.empty[(String, Int)])
-  for step <- steps
-  do
-    step match
-      case s"$label-" =>
-        val hash      = hashValue(label)
-        val box       = boxes(hash)
-        val lensIndex = box.indexWhere((l, _) => l == label)
-        if lensIndex != -1 then
-          boxes(hash) = box.take(lensIndex) ++ box.drop(lensIndex + 1)
+  steps.foreach:
+    case s"$label-"        => boxes(hashValue(label)).remove(label)
+    case s"$label=$focLen" => boxes(hashValue(label))(label) = focLen.toInt
 
-      case s"$label=$focalLength" =>
-        val hash             = hashValue(label)
-        val focalLengthValue = focalLength.toInt
-        val box              = boxes(hash)
-        val lensIndex        = box.indexWhere((l, _) => l == label)
-        if lensIndex != -1 then
-          boxes(hash) = box.updated(lensIndex, (label, focalLengthValue))
-        else boxes(hash) = box :+ (label, focalLengthValue)
+  val lensFocusingPowers = for
+    (box, boxNumber)              <- boxes.zipWithIndex
+    ((_, focalLength), slotIndex) <- box.zipWithIndex
+  yield (boxNumber + 1) * (slotIndex + 1) * focalLength
 
-  boxes
+  lensFocusingPowers.sum
--- src/test/scala/day15.scala
+++ src/test/scala/day15.scala
@@ -4,8 +4,6 @@ import day15.*
 given Day = Day(15)
 class Test extends DayTest:
   def parts = Seq(
-    Part(hashValuesSum, "example"    -> 1320, "input" -> 511215),
-    Part(focusingPowerSum, "example" -> 145, "input"  -> 0),
+    Part(hashValuesSum, "example" -> 1320, "input" -> 511215),
+    Part(focusingPower, "example" -> 145, "input"  -> 236057),
   )
-
-  hashValue.testCases("HASH" :> "HASH" -> 52)
