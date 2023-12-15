5f40aa0667cff9e6e028d600464f6d68a9f36c88
Fabian Achammer <fabian@achammer.dev>
2023-12-15 06:41:28 +0100

    solve day15 part 2

--- src/main/scala/day15.scala
+++ src/main/scala/day15.scala
@@ -1,7 +1,5 @@
 package day15
 
-import debug.*
-
 // part 1
 def hashValuesSum(input: String) = input.strip.split(',').map(hashValue).sum
 
@@ -10,3 +8,37 @@ def hashValue(input: String) =
     .map(_.toInt)
     .fold(0): (current, next) =>
       ((current + next) * 17) % 256
+
+// part 2
+def focusingPowerSum(input: String) =
+  initialize(input).zipWithIndex
+    .map((box, boxNumber) =>
+      box.zipWithIndex.map { case ((_, focalLength), slotIndex) =>
+        (boxNumber + 1) * (slotIndex + 1) * focalLength
+      }.sum,
+    )
+    .sum
+
+def initialize(input: String): Array[Vector[(String, Int)]] =
+  val steps = input.strip.split(',')
+  val boxes = Array.fill(256)(Vector.empty[(String, Int)])
+  for step <- steps
+  do
+    step match
+      case s"$label-" =>
+        val hash      = hashValue(label)
+        val box       = boxes(hash)
+        val lensIndex = box.indexWhere((l, _) => l == label)
+        if lensIndex != -1 then
+          boxes(hash) = box.take(lensIndex) ++ box.drop(lensIndex + 1)
+
+      case s"$label=$focalLength" =>
+        val hash             = hashValue(label)
+        val focalLengthValue = focalLength.toInt
+        val box              = boxes(hash)
+        val lensIndex        = box.indexWhere((l, _) => l == label)
+        if lensIndex != -1 then
+          boxes(hash) = box.updated(lensIndex, (label, focalLengthValue))
+        else boxes(hash) = box :+ (label, focalLengthValue)
+
+  boxes
--- src/test/scala/day15.scala
+++ src/test/scala/day15.scala
@@ -3,6 +3,9 @@ import test.*
 import day15.*
 given Day = Day(15)
 class Test extends DayTest:
-  def parts = Seq(Part(hashValuesSum, "example" -> 1320, "input" -> 511215))
+  def parts = Seq(
+    Part(hashValuesSum, "example"    -> 1320, "input" -> 511215),
+    Part(focusingPowerSum, "example" -> 145, "input"  -> 0),
+  )
 
   hashValue.testCases("HASH" :> "HASH" -> 52)
