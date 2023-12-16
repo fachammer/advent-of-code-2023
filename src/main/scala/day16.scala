201fc551eed94304f4b125eb24a118b88423247f
Fabian Achammer <fabian@achammer.dev>
2023-12-16 09:16:37 +0100

    refactor day16

--- src/main/scala/day16.scala
+++ src/main/scala/day16.scala
@@ -1,117 +1,69 @@
 package day16
 
-import scala.collection.mutable.Stack
-
 // part 1
-type Cave = Vector[Vector[Char]]
-case class Position(col: Int, row: Int)
-enum Direction:
-  case Up
-  case Left
-  case Down
-  case Right
+case class Cave(cave: Vector[Vector[Char]]):
+  def at(position: Position) = cave(position.row)(position.col)
+  def cols                   = 0 until cave(0).length
+  def rows                   = 0 until cave.length
+  def isInBounds(position: Position) =
+    rows.contains(position.row) && cols.contains(position.col)
 
+object Cave:
+  def parse(input: String) = Cave(input.linesIterator.map(_.toVector).toVector)
+
+case class Position(col: Int, row: Int):
+  def +(d: Direction) = Position(col + d.col, row + d.row)
+
+case class Direction(col: Int, row: Int):
+  def opposite      = Direction(-col, -row)
+  def flipUpRight   = Direction(-row, -col)
+  def flipDownRight = flipUpRight.opposite
+  def orthogonals   = Seq(flipUpRight, flipDownRight)
+object Direction:
+  val Up    = Direction(0, -1)
+  val Left  = Direction(-1, 0)
+  val Down  = Direction(0, 1)
+  val Right = Direction(1, 0)
 import Direction.*
 
+case class Beam(position: Position, direction: Direction):
+  def directTowards(dir: Direction) = Beam(position + dir, dir)
+
+  def step          = directTowards(direction)
+  def flipUpRight   = directTowards(direction.flipUpRight)
+  def flipDownRight = directTowards(direction.flipDownRight)
+  def split         = direction.orthogonals.map(directTowards)
+  def isHorizontal  = direction.row == 0
+  def isVertical    = direction.col == 0
+
 def numberOfEnergizedTiles(input: String) =
-  val cave = input.linesIterator.map(_.toVector).toVector
-  energizedTiles(cave, Beam.fresh(0, 0, Right)).size
-
-extension (cave: Cave)
-  def apply(col: Int, row: Int) = cave(row)(col)
-  def isInBounds(col: Int, row: Int) =
-    (0 until cave.length).contains(row)
-      && (0 until cave(0).length).contains(col)
-
-var id = 0
-def nextId =
-  id += 1
-  id
-
-case class Beam(id: Int, startCol: Int, startRow: Int, direction: Direction):
-  def position = Position(startCol, startRow)
-  def nextPosition =
-    val (col, row) = direction match
-      case Up    => (startCol, startRow - 1)
-      case Left  => (startCol - 1, startRow)
-      case Right => (startCol + 1, startRow)
-      case Down  => (startCol, startRow + 1)
-    Position(col, row)
-
-  def positionDirection = (position, direction)
-
-object Beam:
-  def fresh(startCol: Int, startRow: Int, direction: Direction): Beam =
-    Beam(nextId, startCol, startRow, direction)
+  energizedTiles(Cave.parse(input), Beam(Position(0, 0), Right)).size
 
 def energizedTiles(cave: Cave, initialBeam: Beam): Set[Position] =
+  extension (beam: Beam)
+    def nextBeams = cave.at(beam.position) match
+      case '/'                      => Seq(beam.flipUpRight)
+      case '\\'                     => Seq(beam.flipDownRight)
+      case '|' if beam.isHorizontal => beam.split
+      case '-' if beam.isVertical   => beam.split
+      case _                        => Seq(beam.step)
 
-  import scala.collection.mutable
-  val stack          = Stack(initialBeam)
-  val visitedBeams   = mutable.Set.empty[(Position, Direction)]
-  val energizedTiles = mutable.Set.empty[Position]
+  val beamsToProcess = scala.collection.mutable.Stack(initialBeam)
+  val visitedBeams   = scala.collection.mutable.Set.empty[Beam]
+  while beamsToProcess.nonEmpty do
+    val beam = beamsToProcess.pop()
+    if cave.isInBounds(beam.position) && !visitedBeams.contains(beam) then
+      visitedBeams.add(beam)
+      beamsToProcess.pushAll(beam.nextBeams)
 
-  while stack.nonEmpty do
-    val beam = stack.pop()
-
-    if !visitedBeams.contains((beam.position, beam.direction))
-      && cave.isInBounds(beam.startCol, beam.startRow)
-    then
-      visitedBeams.add(beam.positionDirection)
-      energizedTiles.add(beam.position)
-
-      (cave(beam.startCol, beam.startRow), beam.direction) match
-        case ('.', _) | ('|', Up) | ('|', Down) | ('-', Left) | ('-', Right) =>
-          stack.push(
-            Beam(
-              beam.id,
-              beam.nextPosition.col,
-              beam.nextPosition.row,
-              beam.direction,
-            ),
-          )
-
-        case ('|', Left) | ('|', Right) =>
-          val upBeam   = Beam.fresh(beam.startCol, beam.startRow - 1, Up)
-          val downBeam = Beam.fresh(beam.startCol, beam.startRow + 1, Down)
-          stack.push(upBeam)
-          stack.push(downBeam)
-        case ('-', Up) | ('-', Down) =>
-          val leftBeam  = Beam.fresh(beam.startCol - 1, beam.startRow, Left)
-          val rightBeam = Beam.fresh(beam.startCol + 1, beam.startRow, Right)
-          stack.push(leftBeam)
-          stack.push(rightBeam)
-
-        case ('/', Up) =>
-          stack.push(Beam(beam.id, beam.startCol + 1, beam.startRow, Right))
-        case ('/', Left) =>
-          stack.push(Beam(beam.id, beam.startCol, beam.startRow + 1, Down))
-        case ('/', Down) =>
-          stack.push(Beam(beam.id, beam.startCol - 1, beam.startRow, Left))
-        case ('/', Right) =>
-          stack.push(Beam(beam.id, beam.startCol, beam.startRow - 1, Up))
-
-        case ('\\', Up) =>
-          stack.push(Beam(beam.id, beam.startCol - 1, beam.startRow, Left))
-        case ('\\', Left) =>
-          stack.push(Beam(beam.id, beam.startCol, beam.startRow - 1, Up))
-        case ('\\', Down) =>
-          stack.push(Beam(beam.id, beam.startCol + 1, beam.startRow, Right))
-        case ('\\', Right) =>
-          stack.push(Beam(beam.id, beam.startCol, beam.startRow + 1, Down))
-
-        case _ => ???
-
-  energizedTiles.toSet
+  visitedBeams.map(_.position).toSet
 
 // part 2
 def maxNumberOfEnergizedTiles(input: String) =
-  val cave       = input.linesIterator.map(_.toVector).toVector
-  val rightBeams = (0 until cave.length).map(Beam.fresh(0, _, Right))
-  val leftBeams =
-    (0 until cave.length).map(Beam.fresh(cave(0).length - 1, _, Left))
-  val upBeams = (0 until cave(0).length).map(Beam.fresh(_, cave.length - 1, Up))
-  val downBeams = (0 until cave(0).length).map(Beam.fresh(_, 0, Down))
+  val cave = Cave.parse(input)
+  val boundaryBeams = cave.rows.map(r => Beam(Position(0, r), Right))
+    ++ cave.rows.map(r => Beam(Position(cave.cols.last, r), Left))
+    ++ cave.cols.map(c => Beam(Position(c, cave.rows.last), Up))
+    ++ cave.cols.map(c => Beam(Position(c, 0), Down))
 
-  val startBeams = rightBeams ++ leftBeams ++ upBeams ++ downBeams
-  startBeams.map(energizedTiles(cave, _).size).max
+  boundaryBeams.map(energizedTiles(cave, _).size).max
