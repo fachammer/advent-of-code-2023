29ad9d2d6c7b3d3f154ce679452c6ba6b407e9de
Fabian Achammer <fabian@achammer.dev>
2023-12-17 13:36:13 +0100

    refactor and optimize day17

--- src/main/scala/day17.scala
+++ src/main/scala/day17.scala
@@ -1,90 +1,81 @@
 package day17
 
 // part 1
-case class Grid(grid: Vector[Vector[Int]]):
-  def at(col: Int, row: Int)         = grid(row)(col)
-  def height                         = grid.length
-  def width                          = grid(0).length
-  def cols                           = 0 until width
-  def rows                           = 0 until height
-  def isInBounds(col: Int, row: Int) = cols.contains(col) && rows.contains(row)
-  def neighborhood4(col: Int, row: Int) = neighborhood4Offset(col, row, 1)
+def minimumHeatLossWithStandardCrucible(input: String) =
+  minimumHeatLoss(input, 1, 3)
 
-  def neighborhood4Offset(col: Int, row: Int, offset: Int) =
-    Seq(
-      (col + offset, row),
-      (col, row + offset),
-      (col - offset, row),
-      (col, row - offset),
-    ).filter(isInBounds.tupled)
+// part 2
+def minimumHeatLossWithUltraCrucible(input: String) =
+  minimumHeatLoss(input, 4, 10)
+
+case class Grid(grid: Vector[Vector[Int]]):
+  def at(position: Position) = grid(position.row)(position.col)
+  def height                 = grid.length
+  def width                  = grid(0).length
+  def cols                   = 0 until width
+  def rows                   = 0 until height
+  def isInBounds(position: Position) =
+    cols.contains(position.col) && rows.contains(position.row)
+
 object Grid:
   def parse(input: String): Grid =
     Grid(input.linesIterator.map(_.map(_.asDigit).toVector).toVector)
 
-type Position = (Int, Int)
-extension (pos: Position)
-  def col                       = pos._1
-  def row                       = pos._2
-  def -(other: Position)        = (pos._1 - other._1, pos._2 - other._2)
-  def dot(other: Position): Int = pos._1 * other._1 + pos._2 * other._2
-  def isOrthogonalTo(other: Position): Boolean = dot(other) == 0
-  def manhattanLength: Int                     = col.abs + row.abs
+case class Direction(col: Int, row: Int):
+  def orthogonals = Seq(Direction(-row, col), Direction(row, -col))
+case class Position(col: Int, row: Int):
+  def +(other: Direction) = Position(col + other.col, row + other.row)
+case class Node(position: Position, direction: Option[Direction], length: Int)
 
-case class Node(last2: Seq[Position]):
-  def position = last2.head
+def minimumHeatLoss(input: String, minSteps: Int, maxStepsUntilTurn: Int) =
+  val grid      = Grid.parse(input)
+  val adjacency = adjacent(grid, minSteps, maxStepsUntilTurn)
+  def isDestination(node: Node) =
+    node.length >= minSteps
+      && node.position == Position(grid.cols.last, grid.rows.last)
 
-def minimumHeatLossWithStandardCrucible(input: String) =
-  minimumHeatLoss(input, 1 to 3)
+  dijkstra(adjacency, isDestination, Node(Position(0, 0), None, 0)).get
 
-// part 2
-def minimumHeatLossWithUltraCrucible(input: String) =
-  minimumHeatLoss(input, 4 to 10)
+def adjacent(grid: Grid, minSteps: Int, maxStepsUntilTurn: Int)(node: Node) =
+  val adjacents = scala.collection.mutable.Buffer.empty[Node]
 
-def minimumHeatLoss(input: String, movementRange: Range) =
-  val grid = Grid.parse(input)
-  dijkstra[Node](adjacentNodes(grid, movementRange))(Node(Seq((0, 0)))).flatMap:
-    (k, v) => Option.when(k.position == (grid.cols.last, grid.rows.last))(v)
-  .min
+  if node.direction.isEmpty then
+    adjacents.appendAll:
+      Seq(Direction(0, 1), Direction(1, 0)).map: direction =>
+        Node(node.position + direction, Some(direction), 1)
+  else
+    if node.length < maxStepsUntilTurn then
+      adjacents.append:
+        Node(
+          node.position + node.direction.get,
+          node.direction,
+          node.length + 1,
+        )
 
-def adjacentNodes(grid: Grid, offsets: Iterable[Int])(node: Node) =
-  val current @ (col, row) = node.position
-  val possibleNeighbors = for
-    offset   <- offsets.toSet
-    neighbor <- grid.neighborhood4Offset(col, row, offset)
-  yield neighbor
+    if node.length >= minSteps then
+      node.direction.get.orthogonals.foreach: direction =>
+        val nextPosition = node.position + direction
+        adjacents.append(Node(nextPosition, Some(direction), 1))
 
-  val adjacent = node.last2 match
-    case (col, row) :: Nil => possibleNeighbors
-    case (current :: previous :: Nil) =>
-      possibleNeighbors.filter: neighbor =>
-        (neighbor - current).isOrthogonalTo(current - previous)
+  adjacents
+    .filter(node => grid.isInBounds(node.position))
+    .map(node => (node, grid.at(node.position)))
 
-  def heatLossInStraightLine(from: Position, toPos: Position) =
-    val difference = toPos - from
-    val colOffset  = if difference.col.sign == 0 then 1 else difference.col.sign
-    val rowOffset  = if difference.row.sign == 0 then 1 else difference.row.sign
-    val colRange   = from.col to toPos.col by colOffset
-    val rowRange   = from.row to toPos.row by rowOffset
-    val heatLosses = for col <- colRange; row <- rowRange
-    yield grid.at(col, row)
-    heatLosses.sum - grid.at.tupled(from)
-
-  adjacent.map: neighbor =>
-    (Node(Seq(neighbor, current)), heatLossInStraightLine(current, neighbor))
-
-def dijkstra[T](adjacent: T => Iterable[(T, Int)])(start: T) =
+def dijkstra[T](
+    adjacent: T => Iterable[(T, Int)],
+    isDestination: T => Boolean,
+    start: T,
+): Option[Int] =
   import scala.collection.mutable
   given Ordering[(T, Int)] = Ordering.by((_, length) => -length)
   val nodesToProcess       = mutable.PriorityQueue((start, 0))
   val visitedNodes         = mutable.HashMap.empty[T, Int]
   while nodesToProcess.nonEmpty do
-    val (node, totalWeight) = nodesToProcess.dequeue()
-    if !visitedNodes.contains(node) || visitedNodes(node) > totalWeight then
-      visitedNodes.put(node, totalWeight)
-      val nextNodes =
-        adjacent(node).map: (n, weight) =>
-          (n, totalWeight + weight)
-      nextNodes.foreach: n =>
-        nodesToProcess.enqueue(n)
+    val (node, totalLength) = nodesToProcess.dequeue()
+    if isDestination(node) then return Some(totalLength)
+    if !visitedNodes.contains(node) || visitedNodes(node) > totalLength then
+      visitedNodes.put(node, totalLength)
+      for (next, weight) <- adjacent(node)
+      do nodesToProcess.enqueue((next, totalLength + weight))
 
-  visitedNodes.toMap
+  None
