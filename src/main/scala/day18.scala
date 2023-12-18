54deac27ea43b8bebcaea88950a934b02df7dcb8
Fabian Achammer <fabian@achammer.dev>
2023-12-18 13:41:31 +0100

    solve day18 part 2

--- src/main/scala/day18.scala
+++ src/main/scala/day18.scala
@@ -4,6 +4,63 @@ import scala.collection.mutable
 
 // part 1
 def part1(input: String) =
+  val steps =
+    for case s"$step $amount $_" <- input.linesIterator
+    yield (step, amount.toInt)
+  lagoonSize(steps)
+
+def part2(input: String) =
+  val steps =
+    for
+      case s"$_ $_ (#$color)" <- input.linesIterator
+      step = color.last match
+        case '0' => "R"
+        case '1' => "D"
+        case '2' => "L"
+        case '3' => "U"
+      amount = Integer.parseInt(color.init, 16)
+    yield (step, amount.toInt)
+  lagoonSize(steps)
+
+def lagoonSize(steps: Iterator[(String, Int)]) =
+  enum Line:
+    case Right(from: Position, length: Int)
+    case Down(from: Position, length: Int)
+    case Left(from: Position, length: Int)
+    case Up(from: Position, length: Int)
+
+    def fromPosition = this match
+      case Right(from, length) => from
+      case Down(from, length)  => from
+      case Left(from, length)  => from
+      case Up(from, length)    => from
+    def toPosition = fromPosition + Direction(
+      direction.col * (lineLength - 1),
+      direction.row * (lineLength - 1),
+    )
+    def direction = this match
+      case Right(from, length) => Direction(1, 0)
+      case Down(from, length)  => Direction(0, 1)
+      case Left(from, length)  => Direction(-1, 0)
+      case Up(from, length)    => Direction(0, -1)
+    def lineLength = this match
+      case Right(from, length) => length
+      case Down(from, length)  => length
+      case Left(from, length)  => length
+      case Up(from, length)    => length
+
+    def extended = this match
+      case Right(from, length) => Right(from - direction, length + 1)
+      case Down(from, length)  => Down(from - direction, length + 1)
+      case Left(from, length)  => Left(from - direction, length + 1)
+      case Up(from, length)    => Up(from - direction, length + 1)
+
+    val minCol = fromPosition.col.min(toPosition.col)
+    val maxCol = fromPosition.col.max(toPosition.col)
+    val minRow = fromPosition.row.min(toPosition.row)
+    val maxRow = fromPosition.row.max(toPosition.row)
+
+  import Line.*
   case class Direction(col: Int, row: Int):
     def rightNormal = Direction(-row, col)
   case class Position(col: Int, row: Int):
@@ -17,64 +74,89 @@ def part1(input: String) =
       Seq((col + 1, row), (col, row + 1), (col - 1, row), (col, row - 1)).map(
         (c, r) => Position(c, r),
       )
-  type Color = String
-  case class Grid(width: Int, height: Int):
-    val grid: Array[Array[Color]] = Array.fill(height, width)(".")
-    val rows                      = 0 until grid.length
-    val cols                      = 0 until grid(0).length
-    def update(position: Position, updated: Color) =
-      val pos = position
-      grid(pos.row)(pos.col) = updated
-    def apply(position: Position) =
-      val pos = position
-      grid(pos.row)(pos.col)
-    def contains(position: Position) =
-      cols.contains(position.col) && rows.contains(position.row)
-    def mkString =
-      val builder = mutable.StringBuilder()
-      for row <- rows do
-        for col <- cols do builder.addAll(grid(row)(col))
-        builder.addOne('\n')
-      builder.mkString
 
-  var position  = Position(0, 0)
-  val positions = mutable.LinkedHashSet(Position(0, 0))
-  for case s"$step $amount (#$color)" <- input.linesIterator do
-    val direction = step match
-      case "R" => Direction(1, 0)
-      case "D" => Direction(0, 1)
-      case "L" => Direction(-1, 0)
-      case "U" => Direction(0, -1)
-    (1 to amount.toInt).foreach: _ =>
-      position += direction
-      positions.add(position)
+  var position = Position(0, 0)
+  val lines    = mutable.Buffer[Line]()
+  for (step, amount) <- steps do
+    val line = step match
+      case "R" => Right(position, amount + 1)
+      case "D" => Down(position, amount + 1)
+      case "L" => Left(position, amount + 1)
+      case "U" => Up(position, amount + 1)
+    lines.append(line)
+    position = line.toPosition
 
-  val minCol          = positions.minBy(_.col).col
-  val maxCol          = positions.maxBy(_.col).col
-  val minRow          = positions.minBy(_.row).row
-  val maxRow          = positions.maxBy(_.row).row
-  val origin          = Direction(minCol, minRow)
-  val grid            = Grid(maxCol - minCol + 1, maxRow - minRow + 1)
-  val offsetPositions = positions.map(_ - origin)
-  offsetPositions.foreach: pos =>
-    grid(pos) = "#"
-
-  val insidePosition = offsetPositions.sliding(2).flatMap { s =>
-    val (a, b)         = (s.head, s.last)
-    val possibleInside = a + (b - a).rightNormal // assuming right-wound path
-    Option.when(s.head.col == s.last.col && grid(possibleInside) == ".")(
-      possibleInside,
+  val sortedLines =
+    lines.sortWith((l, k) =>
+      l.minCol < k.minCol || l.minCol == k.minCol && l.minRow < k.minRow || l.minCol == k.minCol && l.minRow == k.minRow && ((
+        l,
+        k,
+      ) match
+        case (Up(_, _) | Down(_, _), _) => true
+        case _                          => false
+      ),
     )
-  }.next
 
-  def floodFillFrom(point: Position) =
-    val queue = mutable.Queue(point)
-    while queue.nonEmpty do
-      val pos = queue.dequeue()
-      if grid.contains(pos) && grid(pos) == "." then
-        grid(pos) = "#"
-        queue.enqueueAll(pos.neighborhood4)
+  def rowIntersectingLines(row: Int) =
+    sortedLines.filter { line =>
+      (line.minRow to line.maxRow).contains(row)
+    }
 
-  floodFillFrom(insidePosition)
+  val positions = lines.flatMap(l => Seq(l.fromPosition, l.toPosition))
+  val minCol    = positions.minBy(_.col).col
+  val minRow    = positions.minBy(_.row).row
+  val maxRow    = positions.maxBy(_.row).row
+  var area      = 0L
+  for row <- minRow to maxRow do
+    val linesInRow = rowIntersectingLines(row)
+    case class Crossing(col: Int, length: Int, afterwardsInside: Boolean)
 
-  grid.grid.flatten.count(_ == "#")
+    val crossings = mutable.Buffer[Crossing]()
+    var index     = 0
+    var inside    = false
+    while index < linesInRow.length do
+      if index == linesInRow.length - 1 then
+        val line = linesInRow(index)
+        crossings.append(
+          Crossing(line.minCol, line.maxCol - line.minCol + 1, false),
+        )
+        index += 1
+      else
+        val line     = linesInRow(index)
+        val nextLine = linesInRow(index + 1)
+
+        line match
+          case Up(_, _) | Down(_, _) =>
+          case _                     => throw Exception("line must be vertical")
+
+        (line, nextLine) match
+          case (Up(_, _), Down(_, _)) | (Down(_, _), Up(_, _)) =>
+            inside = !inside
+            crossings.append(Crossing(line.minCol, 1, inside))
+            index += 1
+          case (Up(_, _) | Down(_, _), Left(_, _) | Right(_, _)) =>
+            assert(index + 2 < linesInRow.length)
+            linesInRow(index + 2) match
+              case (Up(_, _) | Down(_, _)) =>
+              case _ => throw Exception("next next must be vertical")
+
+            (line, linesInRow(index + 2)) match
+              case (Up(_, _), Up(_, _)) | (Down(_, _), Down(_, _)) =>
+                inside = !inside
+              case _ =>
+
+            crossings.append(
+              Crossing(nextLine.minCol, nextLine.lineLength, inside),
+            )
+            index += 3
+          case _ => throw Exception("should not happen")
+
+    inside = false
+    var col = minCol
+    for crossing <- crossings do
+      if inside then area += (crossing.col - col)
+      inside = crossing.afterwardsInside
+      col = crossing.col + crossing.length
+      area += crossing.length
+
+  area
--- src/test/scala/day18.scala
+++ src/test/scala/day18.scala
@@ -9,4 +9,9 @@ class Test extends DayTest:
       "example" -> 62,
       "input"   -> 35401,
     ),
+    Part(
+      part2,
+      "example" -> 952408144115L,
+      "input"   -> 48020869073824L,
+    ),
   )
