5e5d6b1a81c8144bb6e0df9cf29a82ee82eafb51
Fabian Achammer <fabian@achammer.dev>
2023-12-20 19:57:40 +0100

    add graphviz to dependencies

--- flake.nix
+++ flake.nix
@@ -7,7 +7,15 @@
     let pkgs = nixpkgs.legacyPackages.aarch64-darwin;
     in {
       devShells.aarch64-darwin.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ scala_3 sbt nil nixfmt jdk visualvm ];
+        buildInputs = with pkgs; [
+          scala_3
+          sbt
+          nil
+          nixfmt
+          jdk
+          visualvm
+          graphviz
+        ];
       };
     };
 }
