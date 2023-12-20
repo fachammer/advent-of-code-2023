76528280c3acedff74de0c847329a09387ce8d84
Fabian Achammer <fabian@achammer.dev>
2023-12-20 19:31:14 +0100

    refactor day20

--- src/main/scala/day20.scala
+++ src/main/scala/day20.scala
@@ -1,117 +1,101 @@
 package day20
 
-import debug.*
 import scala.collection.mutable
 
 // part 1
 def part1(input: String) =
   val pulseNetwork = PulseNetwork.parse(input)
   val (low, high) =
-    (1 to 1000).map(_ => pulseNetwork.pushButton("")).foldLeft((0, 0)):
-      case ((accLow, accHigh), (low, high)) => (accLow + low, accHigh + high)
-      case _                                => ???
+    (1 to 1000).map(_ => pulseNetwork.pushButton()).foldLeft((0, 0)):
+      case ((lowAcc, highAcc), sentPulses) =>
+        val (lowPulses, highPulses) =
+          sentPulses.partition((_, p, _) => p == Pulse.Low)
+        (lowAcc + lowPulses.size, highAcc + highPulses.size)
+
   low * high
 
 enum Pulse:
   case Low, High
+
 case class PulseNetwork(
     outputs: mutable.HashMap[String, mutable.Buffer[String]],
     inputs: mutable.HashMap[String, mutable.Buffer[String]],
     moduleTypes: mutable.HashMap[String, ModuleType],
 ):
-  private val states = moduleTypes.map {
-    case (name, ModuleType.FlipFlop) => (name, ModuleState.FlipFlop(false))
-    case (name, ModuleType.Conjunction) =>
-      (
-        name,
-        ModuleState.Conjunction(
-          mutable.HashMap(inputs(name).map(i => (i, true)).toSeq*),
-        ),
-      )
-    case (name, _) => (name, ModuleState.None)
-  }
-
-  val lastIncomingPulses = mutable.HashMap[String, Pulse]()
-
-  def stateView = states.toMap.view
-
-  def pushButton(notifyIfLowOn: String): (Int, Int) | "found" =
-    val queue          = mutable.Queue(("button", true, "broadcaster"))
-    var lowPulsesSent  = 0
-    var highPulsesSent = 0
+  def pushButton(): Seq[(String, Pulse, String)] =
+    val queue      = mutable.Queue(("button", Pulse.Low, "broadcaster"))
+    val pulsesSent = mutable.Buffer[(String, Pulse, String)]()
     while queue.nonEmpty do
-      val (pulseSource, pulseIsLow, pulseDestination) = queue.dequeue()
-      lastIncomingPulses(pulseDestination) =
-        if pulseIsLow then Pulse.Low else Pulse.High
-
-      // println:
-      /* s"$pulseSource -${if pulseIsLow then "low" else "high"}->
-       * $pulseDestination" */
-
-      if pulseIsLow then lowPulsesSent += 1
-      else highPulsesSent += 1
+      val p @ (pulseSource, pulse, pulseDestination) = queue.dequeue()
+      pulsesSent.append(p)
 
       if moduleTypes.contains(pulseDestination) then
-        assert(states.contains(pulseDestination))
-        (moduleTypes(pulseDestination), states(pulseDestination)) match
-          case (ModuleType.FlipFlop, ModuleState.FlipFlop(on)) =>
-            if pulseIsLow then
-              states(pulseDestination) = ModuleState.FlipFlop(!on)
+        assert(moduleTypes.contains(pulseDestination))
+        moduleTypes(pulseDestination) match
+          case ModuleType.FlipFlop(on) =>
+            if pulse == Pulse.Low then
+              moduleTypes(pulseDestination) = ModuleType.FlipFlop(!on)
+              val outPulse = if on then Pulse.Low else Pulse.High
               for output <- outputs(pulseDestination) do
-                queue.enqueue((pulseDestination, on, output))
+                queue.enqueue((pulseDestination, outPulse, output))
 
-          case (ModuleType.Conjunction, ModuleState.Conjunction(inputs)) =>
-            inputs(pulseSource) = pulseIsLow
-            val isOutgoingPulseLow = inputs.forall((_, isLow) => !isLow)
+          case ModuleType.Conjunction(inputs) =>
+            inputs(pulseSource) = pulse
+            val outPulse =
+              if inputs.forall((_, pulse) => pulse == Pulse.High) then Pulse.Low
+              else Pulse.High
             for output <- outputs(pulseDestination) do
-              queue.enqueue((pulseDestination, isOutgoingPulseLow, output))
+              queue.enqueue((pulseDestination, outPulse, output))
 
-          case (ModuleType.Broadcaster, ModuleState.None) =>
+          case ModuleType.Broadcaster =>
             for output <- outputs(pulseDestination) do
-              queue.enqueue((pulseDestination, pulseIsLow, output))
+              queue.enqueue((pulseDestination, pulse, output))
 
-          case _ => ???
-      if pulseDestination == notifyIfLowOn && pulseIsLow then return "found"
+    pulsesSent.toSeq
 
-    (lowPulsesSent, highPulsesSent)
+  def reset() =
+    moduleTypes.foreach:
+      case (module, ModuleType.Broadcaster) =>
+        moduleTypes(module) = ModuleType.Broadcaster
+      case (module, ModuleType.FlipFlop(_)) =>
+        moduleTypes(module) = ModuleType.FlipFlop(false)
+      case (module, ModuleType.Conjunction(inputs)) =>
+        moduleTypes(module) = ModuleType.Conjunction:
+          mutable.HashMap(inputs.map((k, _) => (k, Pulse.Low)).toSeq*)
+    this
 
-  def buttonPressesUntilRxIsLow =
-    assert(inputs("rx").size == 1)
-    val rxInput = inputs("rx").head
-    assert(moduleTypes(rxInput) == ModuleType.Conjunction)
-    buttonPressesUntilStateFromDefault(
-      Map(
-        rxInput ->
-          ModuleState.Conjunction(
-            mutable.HashMap(inputs(rxInput).map(name => (name, false)).toSeq*),
-          ),
-      ),
-    )
+  def buttonPressesUntilLow(module: String): Long =
+    module match
+      case "rx" =>
+        /* we assume here that rx's only input is a conjunction whose inputs are
+         * all inverters (we call them the inverter inputs).
+         * furthermore, without checking, we also assume that the inverter
+         * inputs are low after every number of button presses which is a
+         * multiple of buttonPressesUntilLow(inverter input).
+         * this makes sure that we can take the lcm of all the
+         * buttonPressesUntilLow(inverter input) to get the correct result */
+        assert(inputs("rx").size == 1)
+        val finalConjunction = inputs("rx").head
+        val disjointedButtonPresses = inputs(finalConjunction).map: input =>
+          moduleTypes(input) match
+            case ModuleType.Conjunction(_) =>
+            case _                         => assert(false)
+          assert(inputs(input).size == 1)
+          buttonPressesUntilLow(input)
+        disjointedButtonPresses.foldLeft(1L)(lcm)
 
-  def modules =
-    inputs.map((name, inputs) =>
-      Module(
-        name,
-        moduleTypes.get(name),
-        states.get(name).getOrElse(ModuleState.None),
-        inputs.toSeq,
-        outputs.get(name).map(_.toSeq).getOrElse(Seq.empty),
-      ),
-    )
+      case _ =>
+        var buttonPresses    = 0L
+        val pulseNetworkCopy = this.copy().reset()
+        var pulsesSent       = Seq.empty[(String, Pulse, String)]
+        Iterator.iterate(Seq.empty): pulsesSent =>
+          pulseNetworkCopy.pushButton()
+        while !pulsesSent.exists((_, p, to) => to == module && p == Pulse.Low)
+        do
+          buttonPresses += 1
+          pulsesSent = pulseNetworkCopy.pushButton()
 
-  def buttonPressesUntilStateFromDefault(states: Map[String, ModuleState]) =
-    ???
-
-  def printFrom(module: String, depth: Int = 0): Unit =
-    if depth > 10 then return
-
-    val indent = " ".repeat(depth)
-    val stringBuilder = mutable.StringBuilder:
-      s"$indent$module (last in pulse: ${lastIncomingPulses.get(module)})\n"
-    println(stringBuilder.mkString)
-    inputs.get(module).getOrElse(Seq.empty).foreach: input =>
-      printFrom(input, depth + 1)
-    // stringBuilder.mkString
+        buttonPresses
 
 object PulseNetwork:
   def parse(input: String) =
@@ -126,63 +110,53 @@ object PulseNetwork:
       line match
         case s"%$name -> $outputModules" =>
           updateInputsOutputs(name, outputModules)
-          moduleTypes.put(name, ModuleType.FlipFlop)
+          moduleTypes.put(name, ModuleType.FlipFlop(false))
         case s"&$name -> $outputModules" =>
           updateInputsOutputs(name, outputModules)
-          moduleTypes.put(name, ModuleType.Conjunction)
+          moduleTypes.put(name, ModuleType.Conjunction(mutable.HashMap.empty))
         case s"broadcaster -> $outputModules" =>
           updateInputsOutputs("broadcaster", outputModules)
           moduleTypes.put("broadcaster", ModuleType.Broadcaster)
 
-    PulseNetwork(outputs, inputs, moduleTypes)
+    inputs("broadcaster") = mutable.Buffer("button")
+    for module <- moduleTypes.keysIterator do
+      moduleTypes(module) match
+        case ModuleType.Conjunction(i) =>
+          val inputStates = inputs(module).map((_, Pulse.Low))
+          inputStates.foreach((name, pulse) => i(name) = pulse)
+
+        case _ =>
+
+    PulseNetwork(outputs, inputs, moduleTypes).reset()
+
+extension (pulseNetwork: PulseNetwork)
+  def mkGraphvizString =
+    val nodes = pulseNetwork.inputs.keys.map: name =>
+      val nodeName = pulseNetwork.moduleTypes.get(name) match
+        case None | Some(ModuleType.Broadcaster) => name
+        case Some(ModuleType.FlipFlop(_))        => s"$name [fontcolor=blue]"
+        case Some(ModuleType.Conjunction(_))     => s"$name [fontcolor=red]"
+      nodeName
+    .mkString("\n")
+    val edges = pulseNetwork.inputs.keys.map: name =>
+      if pulseNetwork.outputs.get(name).isDefined then
+        val outputs =
+          pulseNetwork.outputs.get(name).getOrElse(Seq.empty).mkString(",")
+        s"$name -> $outputs"
+      else ""
+    .mkString("\n")
+
+    s"\ndigraph {$nodes\n$edges}"
 
 enum ModuleType:
-  case FlipFlop, Conjunction, Broadcaster
-
-enum ModuleState:
   case FlipFlop(on: Boolean)
-  case Conjunction(inputs: mutable.HashMap[String, Boolean])
-  case None
-
-case class Module(
-    name: String,
-    moduleType: Option[ModuleType],
-    moduleState: ModuleState,
-    inputs: Seq[String],
-    outputs: Seq[String],
-)
+  case Conjunction(inputs: mutable.HashMap[String, Pulse])
+  case Broadcaster
 
 def part2(input: String): Long =
   val pulseNetwork = PulseNetwork.parse(input)
-  pulseNetwork.inputs("broadcaster") = mutable.Buffer("button")
-  pulseNetwork.moduleTypes("broadcaster") = ModuleType.Broadcaster
-  pulseNetwork.d
-  val nodes = pulseNetwork.inputs.keys.map: name =>
-    val nodeName = pulseNetwork.moduleTypes.get(name) match
-      case None                         => name
-      case Some(ModuleType.Broadcaster) => "broadcaster"
-      case Some(ModuleType.FlipFlop)    => s"$name [fontcolor=blue]"
-      case Some(ModuleType.Conjunction) => s"$name [fontcolor=red]"
-    nodeName
-  .mkString("\n")
-  val edges = pulseNetwork.inputs.keys.map: name =>
-    if pulseNetwork.outputs.get(name).isDefined then
-      s"$name -> ${pulseNetwork.outputs.get(name).getOrElse(Seq.empty).mkString(",")}"
-    else ""
-  .mkString("\n")
+  // println(pulseNetwork.mkGraphvizString)
+  pulseNetwork.buttonPressesUntilLow("rx")
 
-  println(s"\ndigraph {$nodes\n$edges}")
-
-  // pulseNetwork.printFrom("rx")
-  // println(pulseNetwork.mkStringFrom("rx"))
-  // var maxHigh       = 0
-  // var buttonPresses = 0L
-  // while true do
-  //   buttonPresses += 1
-  //   // lcm(3793,3733,4057,3947)
-  //   pulseNetwork.pushButton("zf") match
-  //     case "found"     => return buttonPresses
-  //     case (low, high) =>
-
-  // buttonPresses
-  3793L * 3733 * 4057 * 3947
+def gcd(a: Long, b: Long): Long = if a == 0 then b else gcd(b % a, a)
+def lcm(a: Long, b: Long)       = a * (b / gcd(a, b))
